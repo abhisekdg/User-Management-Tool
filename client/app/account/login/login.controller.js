cde7658ada2ef72152e8dae74a5a482be1abe01d
Harminder Singh <oyeharminder@gmail.com>
2016-06-26 01:13:11 +0530

    added account component and login page routing

---
+++ client/app/account/login/login.controller.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular
+	.module('UMT')
+	.controller('LoginCtrl', function () {
+
+	});
--- client/app/app.js
+++ client/app/app.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var app = angular.module('UMT', ['ui.router']);
-app.config(function ($stateProvider, $urlRouterProvider) {
+app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
 
+	$locationProvider.html5Mode(true);
+	$urlRouterProvider.otherwise('/');
 
 });
